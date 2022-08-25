6b9d76b2bdd0e930f9938081694e6fc8859310e4
Murtuza Bharmal <murtuzabhrmal@gmail.com>
2022-08-26 02:07:39 +0530

    First Commit

---
+++ src/content_option.js
@@ -0,0 +1,175 @@
+const logotext = "MB";
+const meta = {
+    title: "Murtuza Bharmal",
+    description: "React JS Developer & Innovator",
+};
+
+const introdata = {
+    title: "I’m Murtuza Bharmal",
+    animated: {
+        first: "React JS Developer",
+        second: "I love coding",
+        third: "Perfectionist & Hard wroking",
+    },
+    description: "I always aim to gain new experiences in terms of place, industry, work, culture, and society. I believe in attaining consistency, achieving skill excellency, thriving to be best, and always aspiring to fulfil personal and professional goals. At last, to be a trustable human being.",
+    your_img_url: "https://i.ibb.co/kDD7hV7/mbprofile.jpg",
+};
+
+const dataabout = {
+    title: "Who Am I",
+    aboutme: "Born in month of May, a Taurean by nature, I am hardworking and ambitious professional with a kind heart. I am born and brought up in Rajkot (Gujarat) - India. I love travelling, going on adventure trips, nature and of course Gadgets & Cars. In my free time I either prefer to watch movies or sleep ;) I am comfortable with English, Hindi & Gujarati.",
+}
+const workexperience = [{
+        jobtitle: "React JS Developer",
+        where: "Aayaan Data Science Pvt Ltd",
+        date: "Nov 2020 – Jun 2022",
+    },
+    {
+        jobtitle: "Web Developer",
+        where: "Friscon Technologies Pvt Ltd",
+        date: "Jan 2020 – Oct 2020",
+    },
+    {
+        jobtitle: "PhP Developer",
+        where: "Genesis AI Industries Limited",
+        date: "Apr 2015 - Jun 2017",
+    },
+];
+
+const skills = [{
+        name: "React JS",
+        value: 90,
+    },
+    {
+        name: "HTML / CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Mongo DB",
+        value: 75,
+    },
+    {
+        name: "PhP",
+        value: 70,
+    },
+    {
+        name: "Interpersonal Skills",
+        value: 99,
+    },
+    {
+        name: "Teamwork",
+        value: 90,
+    },
+    
+];
+
+// const services = [{
+//         title: "UI & UX Design",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Mobile Apps",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Wordpress Design",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+// ];
+
+const dataportfolio = [{
+    img: "https://i.ibb.co/wp4qrmQ/1.png",
+    description: "eBizz.co.in - Ecomm Portal | Technology Used: Javascript, HTML, CSS, PhP",
+    link: "https://ebizz.co.in",
+},
+// {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description: "2",
+//     link: "#",
+// },
+{
+    img: "https://i.ibb.co/mq8Tc17/3.png",
+    description: "Amtacables.com - Informative Static Website | Technology Used: Javascript, HTML, CSS, PhP",
+    link: "http://amtacables.com",
+},
+// {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description: "4",
+//     link: "#",
+// },
+{
+    img: "https://i.ibb.co/YhnSPx7/5.png",
+    description: "Aayaan.org - Product Selling Portal | Technology Used: REACT Js, HTML, Javascript, MongoDB",
+    link: "https://aayaan.org",
+},
+// {
+//     img: "https://picsum.photos/400/700/?grayscale",
+//     description: "6",
+//     link: "#",
+// },
+
+// {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description: "7",
+//     link: "#",
+// },
+// {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description: "8",
+//     link: "#",
+// },
+{
+    img: "https://i.ibb.co/TBvJR4t/9.png",
+    description: "Startup Project- Informative Static Website | Technology Used: React JS, Javascript, HTML, CSS, MongoDB",
+    link: "https://github.com/Murtuza-Asquare/open-react-startup-project-mb",
+},
+// {
+//     img: "https://picsum.photos/400/550/?grayscale",
+//     description: "10",
+//     link: "#",
+// },
+// {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description: "11",
+//     link: "#",
+// },
+// {
+//     img: "https://picsum.photos/400/700/?grayscale",
+//     description: "12",
+//     link: "#",
+// },
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "murtuzabhrml@gmail.com",
+    YOUR_FONE: "+91-9727799994",
+    description: "Actively looking for a remote position Job",
+     // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_v90b9sn",
+    YOUR_TEMPLATE_ID: "template_wovltd3",
+    YOUR_USER_ID: "YTXAFRMPu1pXWS7LN",
+};
+
+const socialprofils = {
+    github: "https://github.com/Murtuza-Asquare",
+    Whatsapp: "https://wa.me/9727799994",
+    linkedin: "https://www.linkedin.com/in/murtuzabhrml",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    workexperience,
+    skills,
+    // services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
