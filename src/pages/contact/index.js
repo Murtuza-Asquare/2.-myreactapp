b4e17c16cc85df44035245853622a2b20d79a05a
Murtuza Bharmal <murtuzabhrmal@gmail.com>
2022-10-19 16:07:55 +0530

    First Commit

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -93,9 +93,9 @@ export const ContactUs = () => {
             </Alert>
           </Col>
           <Col lg="5" className="mb-5">
-            <h3 className="color_sec py-4">Get in touch</h3>
+            {/* <h3 className="color_sec py-4">Get in touch</h3> */}
             <address>
-              <strong>Email:</strong>{" "}
+              {/* <strong>Email:</strong>{" "} */}
               <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
                 {contactConfig.YOUR_EMAIL}
               </a>
